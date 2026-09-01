e07cfb449433421a489f519ce7037cbccf4bb8fa
Alwyn <joshsiquig12@gmail.com>
2026-09-01 22:13:45 +0800

    commited from Termux

---
+++ Assignment3.java
@@ -0,0 +1,30 @@
+// Defining class named Assignment
+public class Assignment3 {
+    // The main method to run Java program
+    public static void main(String[] args) {
+        // Declares string for first name
+        String firstName = "Alwyn Josh";
+        // Declares string for last name
+        String lastName = "Siquig";
+        // Declares string for address
+        String address = "Urdaneta City";
+        // Declares string for mobile number
+        // String because mobile number is treated like text
+        String mobileNumber = "09064400755";
+        // Declares integer for age as whole number
+        int age = 17;
+        // Declares double variable for GWA with decimals
+        double gwa = 98.00;
+        // Displays first name on screen.
+        System.out.println("First name: " + firstName);
+        // Displays last name on screen.
+        System.out.println("Last name: " + lastName);
+        // Displays address on screen.
+        System.out.println("Address: " + address);
+        // Displays mobile number on screen.
+        System.out.println("Mobile Number: " + mobileNumber);
+        // Displays age on screen.
+        System.out.println("Age: " + age);
+        // Displays GWA on screen.
+        System.out.println("GWA: " + gwa); }
+}
