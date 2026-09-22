59fbe53dfbc7a181cbfbe1d1152d3a81f8d02068
Alwyn <joshsiquig12@gmail.com>
2026-09-22 20:41:22 +0800

    added Activity 1-4

---
+++ secondJava.java
@@ -0,0 +1,16 @@
+public class secondJava {
+    public static void main(String[] args){
+
+        String name = "Alwyn Josh U. Siquig";
+        String address = "Brgy. Poblacion, Urdaneta City, Pangasinan";
+        String contactNumber = "09064400755";
+        int age = 18;
+        String crush = "Karl Vernon C. Cascolan";
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(contactNumber);
+        System.out.println(age);
+        System.out.println(crush);
+    }
+}
